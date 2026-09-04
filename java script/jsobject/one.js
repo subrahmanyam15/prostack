a37e83fd17b2b7774a6f3a4dec3ab3ff0538edbe
subrahmanyam15 <subrahmanyan0562@gmail.com>
2026-09-04 21:31:49 +0530

    javascript object

---
+++ java script/jsobject/one.js
@@ -0,0 +1,8 @@
+let a={}//empty object
+let emp={eid:101,
+         ename:"RG",
+         esal:45000
+        }
+
+console.log(a)
+console.log(emp)
---
+++ java script/jsobject/two.js
@@ -0,0 +1,14 @@
+let eids=[101,101,101,101]
+let emp={
+    'email':'RG@gmail.com',
+    'email':'RG@oracle.com',
+    'email':'RG@ibm.com',
+    'email':'RG@grey.com',
+    'email':'RG@adobe.com',
+    'email':'RG@prostack.com'
+}
+console.log(eids)
+console.log(emp)
+
+// duplicate are allowed 
+// dupilcate keys are not allowed

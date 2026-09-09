620ab665abd77ddf77edda3eef74a2adbc2aa7c1
subrahmanyam15 <subrahmanyan0562@gmail.com>
2026-09-09 12:46:47 +0530

    loops

---
+++ java script/loopstatement/one.js
@@ -0,0 +1,11 @@
+// for of
+let employees=[
+    {eid:101,ename:'Rahul',esal:45000,gender:'Male'},
+    {eid:102,ename:'Sonia',esal:55000,gender:'Female'},
+    {eid:103,ename:'Priya',esal:65000,gender:'Female'},
+    {eid:104,ename:'Modi',esal:75000,gender:'Male'},
+    {eid:105,ename:'Amith',esal:85000,gender:'Male'}
+]
+for( emp of employees){
+    console.log(emp.ename)
+}
---
+++ java script/loopstatement/two.js
@@ -0,0 +1,11 @@
+// for loop
+let employees=[
+    {eid:101,ename:'Rahul',esal:45000,gender:'Male'},
+    {eid:102,ename:'Sonia',esal:55000,gender:'Female'},
+    {eid:103,ename:'Priya',esal:65000,gender:'Female'},
+    {eid:104,ename:'Modi',esal:75000,gender:'Male'},
+    {eid:105,ename:'Amith',esal:85000,gender:'Male'}
+]
+for(let i=0;i<employees.length;i++){
+    console.log(employees[i].ename)
+}
